cc1006d8428324aecc675effff16d8174af102de
Andrey <achernyshev555@yandex.ru>
2024-06-27 16:50:55 +0300

    Additional task
    Additional task 'makeRequests' has been
    completed. Also fulfilled changes in SCSS

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ window.addEventListener("load", function() {
     const buttons = document.querySelectorAll("div.card_button");
     buttons.forEach(function(elem) {
         elem.addEventListener("click", function(event) {
-            str = event.currentTarget.parentElement.getElementsByClassName("card_label")[0].getElementsByTagName("span")[0].innerHTML
+            let str = event.currentTarget.parentElement.getElementsByClassName("card_label")[0].getElementsByTagName("span")[0].innerHTML;
             str = str.replace("<br> ", "\n");
             console.log(str);
         });
