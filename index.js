72c6ddbc913d42b45b786edd1a3e4d63f9992cf1
Andrey <achernyshev555@yandex.ru>
2024-06-28 22:39:26 +0300

    Add new offer button

--- index.js
+++ index.js
@@ -1,48 +1,53 @@
 import './pre_styles.scss'
 
-const card_amount = Math.floor(Math.random()*7 + 1);
-const cards = Array(card_amount).fill(null);
-    
-for (let i = 0; i < card_amount; i++) {
-    const card = {
-        id: Math.floor(Math.random()*100),
-        name: 'Вклад ' + String(Math.floor(Math.random()*100)),
-        logo: 'img/alpha.svg',
-        rate: String(Math.floor(Math.random()*200+30)/10) + '%', // ставка
-        period: String(Math.floor(Math.random()*335+30)) + ' дн.', // срок
-        amount_min: String(100000) + ' ₽', // сумма
-        amount_max: String(100000 * Math.floor(Math.random()*30)) + ' ₽',
-        url: '1', // для кнопки
-    }; 
-    cards[i] = card;
+const cards = Array(3).fill(null);
+
+for (let i = 0; i < 3; i++) {
+    const card_amount = Math.floor(Math.random()*7 + 1);
+    const cards_slice = Array(card_amount);
+    for (let j = 0; j < card_amount; j++) {
+        const card = {
+            id: Math.floor(Math.random()*100),
+            name: 'Вклад ' + String(Math.floor(Math.random()*100)),
+            logo: 'img/alpha.svg',
+            rate: String(Math.floor(Math.random()*200+30)/10) + '%', // ставка
+            period: String(Math.floor(Math.random()*335+30)) + ' дн.', // срок
+            amount_min: String(100000) + ' ₽', // сумма
+            amount_max: String(100000 * Math.floor(Math.random()*30)) + ' ₽',
+            url: '1', // для кнопки
+        }; 
+        cards_slice[j] = card;
+    }
+    cards[i] = cards_slice;
 }
 
 window.addEventListener("load", function() {
     const divs = document.querySelectorAll('.cards');
     
-    const html = cards.map((card) => 
-        `<div class="card">
-            <div class="card_label">
-                <img class="card_label-img" src="${card.logo}">
-                <span>${card.name}</span>
-            </div>
-            <div class="offer-param">
-                <div class="offer-param_text">Ставка, %</div>
-                <span class="offer-param_value offer-param_percent">от ${card.rate}</span>
-            </div>
-            <div class="offer-param">
-                <span class="offer-param_text">Срок</span>
-                <span class="offer-param_value">${card.period}</span>
-            </div>
-            <div class="offer-param">
-                <span class="offer-param_text">Сумма</span>
-                <span class="offer-param_value">${card.amount_min}—${card.amount_max}</span>
-            </div>
-            <div class="card_button">Открыть вклад</div>
-        </div>`).join('\n');
-    
-    console.log(divs);
-    divs[0].innerHTML = html;
+    for (let i = 0; i < 3; i++) {
+        const cards_slice = cards[i];
+        const html = cards_slice.map((card) => 
+            `<div class="card">
+                <div class="card_label">
+                    <img class="card_label-img" src="${card.logo}">
+                    <span>${card.name}</span>
+                </div>
+                <div class="offer-param">
+                    <div class="offer-param_text">Ставка, %</div>
+                    <span class="offer-param_value offer-param_percent">от ${card.rate}</span>
+                </div>
+                <div class="offer-param">
+                    <span class="offer-param_text">Срок</span>
+                    <span class="offer-param_value">${card.period}</span>
+                </div>
+                <div class="offer-param">
+                    <span class="offer-param_text">Сумма</span>
+                    <span class="offer-param_value">${card.amount_min}—${card.amount_max}</span>
+                </div>
+                <div class="card_button">Открыть вклад</div>
+            </div>`).join('\n');
+        divs[i].innerHTML = html;
+    }
 
     const buttons = document.querySelectorAll('.card_button');
     buttons.forEach(function(elem) {
@@ -50,6 +55,54 @@ window.addEventListener("load", function() {
             let str = event.currentTarget.parentElement.getElementsByClassName("card_label")[0].getElementsByTagName("span")[0].innerHTML;
             str = str.replace("<br> ", "\n");
             alert(str);
+        });
+    });
+
+    const create_button = document.querySelectorAll('.offers_create-button');
+    create_button.forEach(function(elem) {
+        elem.addEventListener("click", function(event) {
+            let str = event.currentTarget.parentElement.getElementsByClassName("cards")[0].innerHTML;
+            const card = {
+                id: Math.floor(Math.random()*100),
+                name: 'Вклад ' + String(Math.floor(Math.random()*100)),
+                logo: 'img/alpha.svg',
+                rate: String(Math.floor(Math.random()*200+30)/10) + '%', // ставка
+                period: String(Math.floor(Math.random()*335+30)) + ' дн.', // срок
+                amount_min: String(100000) + ' ₽', // сумма
+                amount_max: String(100000 * Math.floor(Math.random()*30)) + ' ₽',
+                url: '1', // для кнопки
+            }; 
+            const html =
+            `<div class="card">
+                <div class="card_label">
+                    <img class="card_label-img" src="${card.logo}">
+                    <span>${card.name}</span>
+                </div>
+                <div class="offer-param">
+                    <div class="offer-param_text">Ставка, %</div>
+                    <span class="offer-param_value offer-param_percent">от ${card.rate}</span>
+                </div>
+                <div class="offer-param">
+                    <span class="offer-param_text">Срок</span>
+                    <span class="offer-param_value">${card.period}</span>
+                </div>
+                <div class="offer-param">
+                    <span class="offer-param_text">Сумма</span>
+                    <span class="offer-param_value">${card.amount_min}—${card.amount_max}</span>
+                </div>
+                <div class="card_button">Открыть вклад</div>
+            </div>`;
+            str += html;
+            event.currentTarget.parentElement.getElementsByClassName("cards")[0].innerHTML = str;
+
+            const buttons = document.querySelectorAll('.card_button');
+            buttons.forEach(function(elem) {
+                elem.addEventListener("click", function(event) {
+                    let str = event.currentTarget.parentElement.getElementsByClassName("card_label")[0].getElementsByTagName("span")[0].innerHTML;
+                    str = str.replace("<br> ", "\n");
+                    alert(str);
+                });
+            });
         });
     });
 });
